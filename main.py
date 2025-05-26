0cacbb211ec664a0553e754b695b33f08cb0c843
Lalit Kumar <143518879+LalitIITM@users.noreply.github.com>
2025-05-26 16:58:58 +0530

    v7

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ from sklearn.tree import DecisionTreeClassifier, plot_tree
 from sklearn import metrics
 
 data = pd.read_csv('data/iris.csv')
-print(data.head())
+#print(data.head())
 
 train, test = train_test_split(data, test_size = 0.4, stratify = data['species'], random_state = 42)
 X_train = train[['sepal_length','sepal_width','petal_length','petal_width']]
@@ -20,7 +20,6 @@ mod_dt = DecisionTreeClassifier(max_depth = 3, random_state = 1)
 mod_dt.fit(X_train,y_train)
 prediction=mod_dt.predict(X_test)
 print('The accuracy of the Decision Tree is',"{:.3f}".format(metrics.accuracy_score(prediction,y_test)))
-
 
 # Save model
 os.makedirs("models", exist_ok=True)
